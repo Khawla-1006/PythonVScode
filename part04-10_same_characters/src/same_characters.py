a0989d4cbd742d432ea2e4704d17c3336ce5b060
khawla-1006 <khawla.ben.in@gmail.com>
2025-01-22 18:49:13 +0100

    same characters

---
+++ part04-10_same_characters/src/same_characters.py
@@ -0,0 +1,9 @@
+# Write your solution here
+def same_chars(string,index1,index2) :
+    if index1 < len(string) and index2 < len(string) :
+        if string[index1] == string[index2]:
+            return True
+    return False
+# You can test your function by calling it within the following block
+if __name__ == "__main__":
+    print(same_chars("coder", 1, 2))
