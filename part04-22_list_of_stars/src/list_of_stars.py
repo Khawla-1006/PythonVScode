e380ff7294d439742f267d4884b126a6ae063167
khawla-1006 <khawla.ben.in@gmail.com>
2025-01-29 13:09:19 +0100

    list_of_stars

---
+++ part04-22_list_of_stars/src/list_of_stars.py
@@ -0,0 +1,7 @@
+# Write your solution here
+def list_of_stars(list : list) : 
+    for integer in list:
+        print(integer * "*")
+
+if __name__ == "__main__" : 
+    list_of_stars([2, 3, 4, 8, 1])
