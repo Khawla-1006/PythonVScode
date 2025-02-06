a8b5fd4d6e984ac30b248160230f6f7ef03bc1d1
khawla-1006 <khawla.ben.in@gmail.com>
2025-02-06 11:22:56 +0100

    longest str

--- part05-01_longest_string/src/longest_string.py
+++ part05-01_longest_string/src/longest_string.py
@@ -1,1 +1,7 @@
 # Write your solution here
+def longest(strings : list) :
+    long_string = strings[0]
+    for string in strings :
+        if len(string) > len(long_string) :
+            long_string = string
+    return long_string
