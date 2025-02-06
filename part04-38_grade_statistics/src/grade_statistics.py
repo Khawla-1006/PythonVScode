56dc37ec676f42469991cb3fed2d6dadfe4ccfc8
khawla-1006 <khawla.ben.in@gmail.com>
2025-02-06 10:30:09 +0100

    grade_statistics updated ver : fixed bugs

--- part04-38_grade_statistics/src/grade_statistics.py
+++ part04-38_grade_statistics/src/grade_statistics.py
@@ -1,27 +1,31 @@
-def statistics(total_pts : list) :
-    pt_average = float(sum(total_pts) / len(total_pts))
-    print(f"Points average: {pt_average}")
+def statistics(total_pts : list, ex_pts : list) :
+    pt_average = round(sum(total_pts) / len(total_pts), 1)
+    print(f"Points average:{pt_average : .1F}")
     grades = []
     grade = 0 
-    for total in total_pts :
-        if total > 27 :
-            grade = 5 
-        elif total > 23 :
-            grade = 4
-        elif total > 20 :
-            grade = 3 
-        elif total > 17 :
-            grade = 2 
-        elif total > 14 :
-            grade = 1 
+    for ex_pt , total in zip(ex_pts , total_pts):
+        if ex_pt >= 10 :
+            if total > 27 :
+                grade = 5 
+            elif total > 23 :
+                grade = 4
+            elif total > 20 :
+                grade = 3 
+            elif total > 17 :
+                grade = 2 
+            elif total > 14 :
+                grade = 1 
+            else :
+                grade = 0
+            grades.append(grade)
         else :
             grade = 0
-        grades.append(grade)
+            grades.append(grade)
     passed = 0
     for grade_result in grades :
         if grade_result > 0 :
             passed += 1
-    pass_percentage = passed / len(grades) * 100
+    pass_percentage = round(passed / len(grades) * 100 , 1)
     print(f"Pass percentage: {pass_percentage}")
     #Grade_distribution
     print("Grade distribution:")
@@ -34,17 +38,16 @@ def statistics(total_pts : list) :
 
 def analyze(exam_pts_list : list, exe_list : list) :
     total_pts = []
-    for pts in exam_pts_list :
-        for exe in exe_list :
-            total_pts.append(pts + round(exe / 10))
-    statistics(total_pts)
+    for pts , exe in zip(exam_pts_list,exe_list) :
+        total_pts.append(pts + round(exe / 10 , 1))
+    statistics(total_pts, exam_pts_list)
     
 def main() :
     exam_pts = []
     number_of_exe =[]
     while True :
         input_string = input("Exam points and exercises completed:")
-        if input_string == " ":
+        if input_string == "":
             break
         spliced = input_string.split()
         exam_pts.append(int(spliced[0]))
