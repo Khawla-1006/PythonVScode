1f4b91954d49b1a165b6be92b576a16db7cc84c4
khawla-1006 <khawla.ben.in@gmail.com>
2025-02-09 14:54:15 +0100

    tic_tac_toe

---
+++ part05-12_tic_tac_toe/src/tic_tac_toe.py
@@ -0,0 +1,12 @@
+# Write your solution here
+def play_turn(game_board : list , x :int, y : int, piece : str) :    
+    if x <= 2 and x >= 0 and y <= 2 and y >= 0 :   
+        if game_board[y][x] == "" :
+            game_board[y][x] = piece
+            return True
+    return False
+
+if __name__ == "__main__" :
+    game_board = [['O', '', 'O'], ['', 'X', ''], ['X', 'O', '']]
+    print(play_turn(game_board, 3, 0, "X"))
+    print(game_board)
